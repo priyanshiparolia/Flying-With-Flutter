17b572242f785804c8fb148417547ff78bdf4fe3
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 21:27:42 +0530

    Update question3.dart

--- Task2/question3.dart
+++ Task2/question3.dart
@@ -1,1 +1,20 @@
 
+import 'dart:io';
+
+void main() 
+{
+    print("Program showcasing birthdays: ");
+    print("_____________________________");
+
+    Map<String, Object> bdate = {"Arushi": "February 14, 2002", "Priyanshi": "March 24, 2001", "Sneha": "August 20, 2001", "Pakhi": "September 13, 2002"};
+    print("\nList of people : \n");
+
+    bdate.forEach((key, value) 
+    {
+    print(key);
+    });
+
+    stdout.write("\nEnter the name of the person who's birthday you wish to know: ");
+    String? name = stdin.readLineSync();
+    print("\n$name's birthdate is ${bdate[name]}.\n");
+}
