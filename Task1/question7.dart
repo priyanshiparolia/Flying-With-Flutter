edd12bf9cdd0b0037f3246ef4e4fb700a24e6d7f
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 20:32:08 +0530

    Update question7.dart
    Added the solution to question #6 from Task 1.

--- Task1/question7.dart
+++ Task1/question7.dart
@@ -1,1 +1,26 @@
+//WAP to check if a number is an armstrong number or not.
 
+import 'dart:io';
+import 'dart:core';
+
+void main() 
+{
+  print("Program to check whether a number is Armstrong or not: ");
+  printf("_____________________________________________________");
+  print("Enter a number to check whether it's Armstrong or not: ");
+  int n = int.parse(stdin.readLineSync()!);
+  int sum=0, num;
+  int temp = n;
+  while (n > 0) {
+    int dig = n % 10;
+    n = (n ~/ 10);
+    int cube = dig*dig*dig;
+    sum = sum + cube;
+    
+  }
+  if (temp == sum) {
+    print("$temp is an Armstrong number!");
+  } else {
+    print("$temp is not an Armstrong number!");
+  }
+}
