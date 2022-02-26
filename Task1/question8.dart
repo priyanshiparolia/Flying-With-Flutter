d01c26b8242a9d872eeabe1b9473e20c5d8054a1
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 20:45:29 +0530

    Update question8.dart

--- Task1/question8.dart
+++ Task1/question8.dart
@@ -1,1 +1,21 @@
+//Q8. WAP to find the maximum between three numbers using the ternary operator.
+
+import 'dart:io';
+
+void main() 
+{
+  print("Program to find the maximum between three numbers using the ternary operator: ");
+  print("_____________________________________________________");
+  
+  print("Enter the three numbers: ");
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
+  int c = int.parse(stdin.readLineSync()!);
+  
+  int max;
+  (a > b && a > c) ? max = a : (b > a && b > c) ? max = b : max = c;
+  print("The maximum of the three numbers is: $max .");
+}
+
+
 
