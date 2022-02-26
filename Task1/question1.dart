2dd2ca0743e953f04b7f33f08a031d9d1aa629b7
Priyanshi Gupta <78092804+priyanshiparolia@users.noreply.github.com>
2022-02-26 20:15:03 +0530

    Update question1.dart

--- Task1/question1.dart
+++ Task1/question1.dart
@@ -1,1 +1,11 @@
-
+//Q1 WAP to check if a number is divisible by 5
+import 'dart:io';
+void main() {
+  print("Enter a number to check its divisibility: ");
+  int? num1 = int.parse(stdin.readLineSync()!); //taking input
+  if (num1 % 5 == 0) {
+    print("$num1 is divisible by 5");
+  } else {
+    print("$num1 is not divisible by 5");
+  }
+}
