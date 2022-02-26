bf77c90bf3b27e0e6405b0bc90680ed57901caa3
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 20:15:35 +0530

    Update question 6.dart
    Added the solution to question #6 from Task 1.

--- Task1/question6.dart
+++ Task1/question6.dart
@@ -1,1 +1,25 @@
+//Q6. WAP to generate Fibonacci series up to Nth term.
+
+import 'dart:io';
+
+void main() 
+{
+  print("Program to display Fibonacci series up to Nth term:");
+  print("___________________________________________________");
+  print("Enter the Nth term (upto which you wish to display the Fibonacci Series) : ");
+  
+  int? n = int.parse(stdin.readLineSync()!);
+  int first = 0; int second = 1;
+  int third;
+  print("Fibonacci terms are: ");
+  stdout.write(" ${first} ${second}");
+  for (int i = 0; i < n - 2; i++) {
+    third = first + second;
+    stdout.write(" ${third}");
+    first = second;
+    second = third;
+  }
+}
+
+
 
