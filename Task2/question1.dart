cb463f4c1d53b693e0484eec01ccf1010dda68c6
Priyanshi Gupta <78092804+priyanshiparolia@users.noreply.github.com>
2022-02-26 21:28:42 +0530

    Update question1.dart

--- Task2/question1.dart
+++ Task2/question1.dart
@@ -1,1 +1,9 @@
-
+i)    option c: int multiply(int a, int b) => a * b;
+ii)   option 2: Async functions 
+iii)  option 3: List 
+iv)   option 3: Error generated
+v)    option 3: print(‘Your name is ‘{name}’); 
+vi)   option 2:	nums.forEach(print); 
+vii)  option 1: Ensures type safety
+      option 2: Reduce code duplication
+      option 4: Can work in a single interface that takes a type parameter.
