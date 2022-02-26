e5613946ce65df92ef650f567097fe3cb8c92eac
Priyanshi Gupta <78092804+priyanshiparolia@users.noreply.github.com>
2022-02-26 20:32:21 +0530

    Update question3.dart

--- Task1/question3.dart
+++ Task1/question3.dart
@@ -1,1 +1,18 @@
+//Write a program (using functions) that asks the user for a long string containing multiple words. Print back to the user the same string in reverse manner.
+import 'dart:io';
+void main() 
+{
+  print("Enter string: ");
+  String str = (stdin.readLineSync()!);
+  print(reverse(str));
+}
 
+String reverse(String str)
+{
+  String temp = "";
+  for (int i = str.length - 1; i >= 0; i--) 
+   {
+     temp += str[i];
+   }
+  return temp;
+}
