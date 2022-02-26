f7e88faf65ce9a5636bd0ffaba6c52916a29c407
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 20:59:52 +0530

    Update question10.dart

--- Task1/question10.dart
+++ Task1/question10.dart
@@ -1,1 +1,24 @@
+//Q10. WAP to a Pyramid star pattern as shown in the figure below.
+import 'dart:io';
 
+void main() 
+{
+  print("Program to check whether a character is an alphabet or not using ternary operator: ");
+  print("__________________________________________________________________________________");
+  print("Enter the number of rows: ");
+
+  int? r = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < r; i++) 
+  {
+    for (int j = (r - i); j > 1; j--) 
+    {
+      stdout.write(" ");
+    }
+    for (int j = 0; j <= i; j++) 
+    {
+      stdout.write("* ");
+    }
+    stdout.writeln();
+  }
+}
