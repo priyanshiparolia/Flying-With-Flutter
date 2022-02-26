00a705b8575f5da11f4df50f458797fbb9c2a83e
Priyanshi Gupta <78092804+priyanshiparolia@users.noreply.github.com>
2022-02-26 20:22:36 +0530

    Update question2.dart

--- Task1/question2.dart
+++ Task1/question2.dart
@@ -1,1 +1,18 @@
 
+//WAP to check if a number is even or odd using functions.
+import 'dart:io';
+void check(int num)
+{
+  if (num % 2 == 0) 
+  {
+    print("number is even");
+  } 
+  else
+    print("number is odd");
+}
+void main()
+{
+  print("Enter number");
+  int? num = int.parse(stdin.readLineSync()!);
+  check(num);
+}
