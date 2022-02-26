ccec55e52cfacd3879df8f68b6f343fc191a7eda
Arushi Rautela <writetoarushirautela@gmail.com>
2022-02-26 21:16:45 +0530

    Update question2.dart

--- Task2/question2.dart
+++ Task2/question2.dart
@@ -1,1 +1,11 @@
+void main()
+{
+    print("Program showcasing only even elements of a list: ");
+    print("________________________________________________");
 
+    List <int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+    print("Given list is: $a");
+
+    a.removeWhere((item) => item%2 != 0);
+    print("List containing only even elements is: $a");
+}
